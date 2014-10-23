df0d5417ebab1bef16569d53ea95fa31d6fdef14
dankomutanon <dankomu@gmail.com>
2014-10-22 20:12:17 -0500

    Version 1.0.4
    Edited Calculate_Weights.cs to output number rounded up to next number
    divsible by 5 to reflect the weights used in the gym.

--- Weightlifting Program/Calculate_Weights.cs
+++ Weightlifting Program/Calculate_Weights.cs
@@ -13,16 +13,18 @@ namespace WindowsFormsApplication1
             //Array of integers containing weights {set 1 weight, set 2 weight, set 3 weight}
             int[] wave_A = new int[3];
             int set1, set2, set3;
+      
 
-            weight = Convert.ToInt16(weight * .90); //calculating max is 90% of true max
+            weight = (int) Math.Round(weight * .90); //calculating max is 90% of true max
 
-            set1 = Convert.ToInt16(weight * .75);
+     
+            set1 = (int)Math.Ceiling((weight * .75) / 5) * 5;   //rounding up to nearest integer divisible by 5 to reflect weights used in the gym
             wave_A[0] = set1;
-            set2 = Convert.ToInt16(weight * .8);
+            set2 = (int)Math.Ceiling((weight * .80) / 5) * 5;
             wave_A[1] = set2;
-            set3 = Convert.ToInt16(weight * .85);
+            set3 = (int)Math.Ceiling((weight * .85) / 5) * 5;
             wave_A[2] = set3;
-
+          
             return wave_A;
         }
 
@@ -32,13 +34,14 @@ namespace WindowsFormsApplication1
             int[] wave_B = new int[3];
             int set1, set2, set3;
 
-            weight = Convert.ToInt16(weight * .90); //calculating max is 90% of true max
+            weight = (int)Math.Round(weight * .90); //calculating max is 90% of true max
 
-            set1 = Convert.ToInt16(weight * .8);
+
+            set1 = (int)Math.Ceiling((weight * .80) / 5) * 5;   //rounding up to nearest integer divisible by 5 to reflect weights used in the gym
             wave_B[0] = set1;
-            set2 = Convert.ToInt16(weight * .85);
+            set2 = (int)Math.Ceiling((weight * .85) / 5) * 5;
             wave_B[1] = set2;
-            set3 = Convert.ToInt16(weight * .9);
+            set3 = (int)Math.Ceiling((weight * .90) / 5) * 5;
             wave_B[2] = set3;
 
             return wave_B;
@@ -50,13 +53,14 @@ namespace WindowsFormsApplication1
             int[] wave_C = new int[3];
             int set1, set2, set3;
 
-            weight = Convert.ToInt16(weight * .90); //calculating max is 90% of true max
+            weight = (int)Math.Round(weight * .90); //calculating max is 90% of true max
 
-            set1 = Convert.ToInt16(weight * .75);
+
+            set1 = (int)Math.Ceiling((weight * .75) / 5) * 5;   //rounding up to nearest integer divisible by 5 to reflect weights used in the gym
             wave_C[0] = set1;
-            set2 = Convert.ToInt16(weight * .85);
+            set2 = (int)Math.Ceiling((weight * .85) / 5) * 5;
             wave_C[1] = set2;
-            set3 = Convert.ToInt16(weight * .95);
+            set3 = (int)Math.Ceiling((weight * .95) / 5) * 5;
             wave_C[2] = set3;
 
             return wave_C;
@@ -68,13 +72,14 @@ namespace WindowsFormsApplication1
             int[] wave_D = new int[3];
             int set1, set2, set3;
 
-            weight = Convert.ToInt16(weight * .90); //calculating max is 90% of true max
+            weight = (int)Math.Round(weight * .90); //calculating max is 90% of true max
 
-            set1 = Convert.ToInt16(weight * .6);
+
+            set1 = (int)Math.Ceiling((weight * .60) / 5) * 5;   //rounding up to nearest integer divisible by 5 to reflect weights used in the gym
             wave_D[0] = set1;
-            set2 = Convert.ToInt16(weight * .65);
+            set2 = (int)Math.Ceiling((weight * .65) / 5) * 5;
             wave_D[1] = set2;
-            set3 = Convert.ToInt16(weight * .7);
+            set3 = (int)Math.Ceiling((weight * .70) / 5) * 5;
             wave_D[2] = set3;
 
             return wave_D;
