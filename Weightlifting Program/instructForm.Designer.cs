b26a595b6ba63f863050711a92f47e9686c3ff87
dankomutanon <dankomu@gmail.com>
2014-11-08 22:10:37 -0600

    Version 1.2.0
    Instructions form includes videos of movements.

--- Weightlifting Program/instructForm.Designer.cs
+++ Weightlifting Program/instructForm.Designer.cs
@@ -32,12 +32,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.instructBack = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 126);
+            this.label1.Location = new System.Drawing.Point(12, 92);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(467, 182);
             this.label1.TabIndex = 0;
@@ -47,7 +50,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(124, 62);
+            this.label2.Location = new System.Drawing.Point(127, 41);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(252, 25);
             this.label2.TabIndex = 1;
@@ -56,19 +59,56 @@
             // 
             // instructBack
             // 
-            this.instructBack.Location = new System.Drawing.Point(208, 339);
+            this.instructBack.Location = new System.Drawing.Point(319, 365);
             this.instructBack.Name = "instructBack";
-            this.instructBack.Size = new System.Drawing.Size(75, 23);
+            this.instructBack.Size = new System.Drawing.Size(60, 23);
             this.instructBack.TabIndex = 2;
-            this.instructBack.Text = "Back";
+            this.instructBack.Text = "BACK";
             this.instructBack.UseVisualStyleBackColor = true;
             this.instructBack.Click += new System.EventHandler(this.backClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Back Squat",
+            "Bench Press",
+            "Deadlift",
+            "Overhead Press"});
+            this.comboBox1.Location = new System.Drawing.Point(187, 338);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.viewVideo);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(184, 302);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tutorial Videos";
             // 
             // instructForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(491, 418);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.instructBack);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -84,5 +124,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button instructBack;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
     }
 }
--- Weightlifting Program/instructForm.cs
+++ Weightlifting Program/instructForm.cs
@@ -17,9 +17,35 @@ namespace WindowsFormsApplication1
             InitializeComponent();
         }
 
-        private void backClick(object sender, EventArgs e)
+        private void backClick(object sender, EventArgs e)      //close this form, return to main form
         {
             this.Close();
         }
-    }
+
+        private void viewVideo(object sender, EventArgs e)      //drop down menu for tutorial videos
+        {
+            int selection = comboBox1.SelectedIndex + 1;
+
+            switch (selection)
+            {
+                case 1:
+                    backSquat squatVid = new backSquat();
+                    squatVid.Show();
+                    break;
+                case 2:
+                    benchPress benchVid = new benchPress();
+                    benchVid.Show();
+                    break;
+                case 3:
+                    deadlift deadliftVid = new deadlift();
+                    deadliftVid.Show();
+                    break;
+                case 4:
+                    overheadPress opVid = new overheadPress();
+                    opVid.Show();
+                    break;
+            }    
+        }
+
+   }
 }
